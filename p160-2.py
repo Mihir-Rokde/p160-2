d45bab22a97e56f350caaf871ff045e0bba6414e
Mihir-Rokde <100846891+Mihir-Rokde@users.noreply.github.com>
2023-08-18 15:52:21 +0530

    Add files via upload

--- p160-2.py
+++ p160-2.py
@@ -20,6 +20,43 @@ my_text=Text(root,height=35,width=80)
 my_text.place(relx=0.5,rely=0.55,anchor=CENTER)
 
 name=""
+def openfile():
+    global name
+    my_text.delete(1.0,END)
+    inputfilename.delete(0,END)
+    html_file=filedialog.askopenfilename(title="Open html File",filetypes=(("html files","*.html"),))
+    print(html_file)
+    name=os.path.basename(html_file)
+    formated_name=name.split(".")[0]
+    inputfilename.insert(END,formatedname)
+    root.title(formatedname)
+    html_file=open(name,'r')
+    paragraph=html_file.read()
+    my_text.insert(END,paragraph)
+    html_file.close()
+    
+def save():
+    inputname=inputfilename.get()
+    file=open(inputname+".html","w")
+    data=my_text.get("1.0",END)
+    print(data)
+    file.write(data)
+    inputfilename.delete(0,END)
+    my_text.delete(1.0,END)
+    messagebox.showinfo("update","successfully saved the file")
+    
+def run_html_file():
+    global name
+    webbrowser.open(name)
+    
+open_btn=Button(root,image=open_img,text="open file",command=openfile)
+open_btn.place(relx=0.05,rely=0.03,anchor=CENTER)
+
+savebtn=Button(root,image=save_img,text="savefile",command=save)
+savebtn.place(relx=0.11,rely=0.03,anchor=CENTER)
+
+exitbtn=Button(root,image=exit_img,text="exitfile",command=run_html_file)
+exitbtn.place(relx=0.17,rely=0.03,anchor=CENTER)
 
 
 root.mainloop()
